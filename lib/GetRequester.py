3ad4afb3bf6436a818b79a7e1ade072db0d67b1a
melrwa <orwamelkizedek@gmail.com>
2025-01-18 01:50:45 +0300

    complete lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,14 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+          # Fetch the raw response from the URL
+        response = requests.get(self.url)
+        return response.content
+
+        
 
     def load_json(self):
-        pass
+        # Parse the raw JSON string into a Python object
+        response_body = self.get_response_body()
+        return json.loads(response_body)
+        
